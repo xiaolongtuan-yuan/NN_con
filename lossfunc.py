cf8fbbe0707113fef01129ac4051cf63076fa631
LRMbbj <lrmbbj@qq.com>
2022-07-30 14:13:53 +0800

    解决了模型的“过拟合”问题 去除了对原始数据的归一化处理 增大了batch size

--- lossfunc.py
+++ lossfunc.py
@@ -2,15 +2,17 @@ import torch
 from torch import nn
 
 
+alpha = 0.1
+
+
 class AvgStdLoss(nn.Module):
     def __init__(self):
         super(AvgStdLoss, self).__init__()
-
+    
     def forward(self, output, target):
         # print('target', target.size(), 'output', output.size())
         # std = torch.div((output[:, 0] - target), torch.exp(output[:, 1]))
         std = torch.div((target - output[:, 0]), output[:, 1])
-        res = torch.abs(torch.mean(std)) + torch.abs(torch.std(std) - 1)
-        # res = torch.abs(torch.mean(std)) + torch.abs(torch.pow(torch.std(std), 2) - 1)
-        # res = torch.abs(torch.mean(std)) + torch.abs(torch.std(std) - 1)
+        # res = alpha * torch.pow(torch.mean(std), 2) + (1-alpha) * torch.pow(torch.std(std) - 1, 2)
+        res = torch.pow(torch.mean(std), 2) + 10 * torch.pow(torch.std(std) - 1, 2)
         return res
