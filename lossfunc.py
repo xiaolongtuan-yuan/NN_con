b05ae3eaaa3937fc34c5b5250c81a57616bc6c91
LRMbbj <lrmbbj@qq.com>
2022-07-30 20:17:52 +0800

    更换了基于最大似然函数的损失函数

--- lossfunc.py
+++ lossfunc.py
@@ -1,18 +1,26 @@
 import torch
 from torch import nn
 
-
-alpha = 0.1
+alpha = 0
 
 
 class AvgStdLoss(nn.Module):
     def __init__(self):
         super(AvgStdLoss, self).__init__()
     
+    # def forward(self, output, target):
+    #     # print('target', target.size(), 'output', output.size())
+    #     # std = torch.div((output[:, 0] - target), torch.exp(output[:, 1]))
+    #     # std = torch.div((target - output[:, 0]), torch.clamp(output[:, 1], 1e-3))
+    #     std = torch.div((target - output[:, 0]), torch.abs(output[:, 1]))
+    #     res = torch.mean(torch.pow(std, 2)) + torch.abs(torch.median(std)) + torch.mul(
+    #         torch.pow(torch.var(std) - 1, 2), torch.mean(torch.abs(output[:, 1])))
+    #     # res = torch.mean(torch.pow(std, 2))
+    #     return res, std
+    
     def forward(self, output, target):
-        # print('target', target.size(), 'output', output.size())
-        # std = torch.div((output[:, 0] - target), torch.exp(output[:, 1]))
-        std = torch.div((target - output[:, 0]), output[:, 1])
-        # res = alpha * torch.pow(torch.mean(std), 2) + (1-alpha) * torch.pow(torch.std(std) - 1, 2)
-        res = torch.pow(torch.mean(std), 2) + 10 * torch.pow(torch.std(std) - 1, 2)
-        return res
+        q = torch.distributions.Normal(loc=output[:, 0], scale=torch.abs(output[:, 1]))
+        neg_log = -1 * torch.mean(q.log_prob(target))
+        std = torch.div((target - output[:, 0]), torch.abs(output[:, 1]))
+
+        return neg_log, std
